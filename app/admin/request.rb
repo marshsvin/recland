51739eb00bb2c02bad2b2c5ddecaaea1aff81baa
Raghu Betina <raghu@firstdraft.com>
2017-05-15 22:31:28 +0000

    Whitelist columns in Request for ActiveAdmin

--- app/admin/request.rb
+++ app/admin/request.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Request do
 
+ permit_params :property_id, :start_date, :end_date, :user_id, :acceptance, :user_review
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
